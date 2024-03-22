d26f5317a697d33c6e066078a67a4cde6797a35b
craaen10 <colin.raaen@gmail.com>
2024-03-21 23:02:27 -0400

    March 21, 2024: Procfile added

--- app.py
+++ app.py
@@ -19,8 +19,11 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+# Configure Heroku Postgres database
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://")
+db = SQL(uri)
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
